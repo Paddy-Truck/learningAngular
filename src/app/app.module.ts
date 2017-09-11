4b47560f0655ae8ecddfc6f23bedde882c741d66
Patrick Robinson <ptrobinson7@gmail.com>
2017-09-11 11:48:05 +0200

    Added NavBar and Footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ServerComponent } from './hold/server/server.component';
+import { ServersComponent } from './hold/servers/servers.component';
+import { NavComponent } from './nav/nav.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ServerComponent,
+    ServersComponent,
+    NavComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
